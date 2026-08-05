5a7bb5d2a2c69929dc2da0ab8d38df0e2f0e0240
vincenico28 <escalavincenico28@gmail.com>
2026-08-05 09:18:44 +0800

    chore: untrack .vercel build artifacts

--- .vercel/output/functions/[...].func/_chunks/empty-plugin-adapters-D9UWiqvJ.mjs
+++
@@ -1,5 +0,0 @@
-//#region dist/server/assets/empty-plugin-adapters-D9UWiqvJ.js
-var pluginSerializationAdapters = [];
-var hasPluginAdapters = false;
-//#endregion
-export { hasPluginAdapters, pluginSerializationAdapters };
